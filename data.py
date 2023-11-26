1592ea730b910fd88728e1de23251dbd4a3d541d
Mehreen <52442337+Mehreen839@users.noreply.github.com>
2023-11-26 18:26:25 +0530

    Add files via upload
    A web page is hosted on the localhost using flask.

---
+++ data.py
@@ -0,0 +1,26 @@
+
+def Articles():
+    articles=[
+        {
+        'id':1,
+        'title':'Article One',
+        'body':'lorem',
+        'author':'Brad Traversy',
+        'create_date':'08-09-2019'
+        },
+         {
+        'id':2,
+        'title':'Article Two',
+        'body':'lorem',
+        'author':'John Traversy',
+        'create_date':'08-09-2020'
+        },
+         {
+        'id':3,
+        'title':'Article Three',
+        'body':'lorem',
+        'author':'Sara Traversy',
+        'create_date':'08-07-2019'
+        }
+    ]
+    return articles
